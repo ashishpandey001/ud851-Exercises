51b4451da2063ce2f70f9935b27d579b8db27663
Ashish Pandey <ashishpandeyone@gmail.com>
2018-03-26 02:32:29 +0530

    completed Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -19,8 +19,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,11 +52,15 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickOpenAddressButton(View v) {
         // TODO (5) Store an address in a String
-
+        String homeAddress = "Golden Park, Kalyan";
         // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
-
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("geo")
+                .path("0,0")
+                .appendQueryParameter("q", homeAddress);
+        Uri homeAddressURI = builder.build();
         // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        showMap(homeAddressURI);
     }
 
     /**
@@ -113,12 +120,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     // TODO (1) Create a method called showMap with a Uri as the single parameter
-    // Do steps 2 - 4 within the showMap method
+    void showMap(Uri geoURI) {
+        // Do steps 2 - 4 within the showMap method
         // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
-
+        Intent geoIntent = new Intent(Intent.ACTION_VIEW);
         // TODO (3) Set the data of the Intent to the Uri passed into this method
-
+        geoIntent.setData(geoURI);
         // TODO (4) Verify that this Intent can be launched and then call startActivity
-
-
+        if (geoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(geoIntent);
+        }
+    }
 }
